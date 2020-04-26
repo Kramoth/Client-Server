ae5042f267ce02bfa55eeeb8efbb893a04a9300e
kramoth <kramoth@udm.ac.mu>
2020-04-26 21:16:13 +0400

    [dev]add custom message from client

--- tcpIP/clientSimple.py
+++ tcpIP/clientSimple.py
@@ -1,9 +1,13 @@
 import socket
 
-HOST='localhost'
-port=12345
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, port))
-sock.send(b"hi")
-data=sock.recv(1024)
-print(data)
+
+if __name__ == '__main__':
+	
+	HOST='localhost'
+	port=12345
+	sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((HOST, port))
+	msg=raw_input("type a msg to send: \n")
+	sock.send(msg.encode())
+	data=sock.recv(1024)
+	print(data)
